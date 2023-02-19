5ffb58f681c6a69ebc16fcaf0b0d6ad3b378a416
Özhan <ozhanaloglu1@gmail.com>
2023-02-20 02:06:16 +0300

    Customer Entity Eklendi.

--- Entities/Concrete/Customer.cs
+++ Entities/Concrete/Customer.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Core.Entities;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +7,10 @@ using System.Threading.Tasks;
 
 namespace Entities.Concrete
 {
-    internal class Customer
+    public   class Customer:IEntity
     {
+        public int CustomerId { get; set; }
+        public string CustomerName { get; set; }
+        public string CustomerSurname { get; set; }
     }
 }
