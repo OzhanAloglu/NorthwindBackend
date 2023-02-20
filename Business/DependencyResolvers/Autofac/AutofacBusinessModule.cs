83671d408006660483e4a0ef187db5e03edcc741
Özhan <ozhanaloglu1@gmail.com>
2023-02-20 16:55:40 +0300

    Jwt eklendi. Altyapı devam ediyor.

--- Business/DependencyResolvers/Autofac/AutofacBusinessModule.cs
+++ Business/DependencyResolvers/Autofac/AutofacBusinessModule.cs
@@ -21,6 +21,9 @@ namespace Business.DependencyResolvers.Autofac
             builder.RegisterType<CategoryManager>().As<ICategoryService>();
             builder.RegisterType<EfCategoryDal>().As<ICategoryDal>();
 
+            builder.RegisterType<UserManager>().As<IUserService>();
+            builder.RegisterType<EfUserDal>().As<IUserDal>();
+
         }
     }
 }
---
+++ Core/Utilities/Security/Jwt/JwtHelper.cs
@@ -0,0 +1,21 @@
+﻿using Core.Entities.Concrete;
+using Microsoft.Extensions.Configuration;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Core.Utilities.Security.Jwt
+{
+    public class JwtHelper : ITokenHelper
+    {
+
+        public IConfiguration Configuration { get;}
+
+        public AccessToken CreateToken(User user, List<OperationClaim> operationClaims)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
